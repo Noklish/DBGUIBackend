e8f73dd407cbffebe720b7286a0bcb425baafecd
Noah Schweser <nschweser@smu.edu>
2018-10-14 18:40:54 -0500

    added get, post, and put statements for basic user account interaction

--- SlimDataPHP/src/routes.php
+++ SlimDataPHP/src/routes.php
@@ -5,10 +5,80 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/[{userID}]', function (Request $request, Response $response, array $args) {
+	$sth = $this->db->prepare("SELECT userId, lastName, firstName, points FROM accounts");
+	$sth->execute();
+	$accounts = $sth->fetchAll();
+	return $this->response->withJson($accounts);
+});
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->post('/accounts', function ($request, $response) {
+	$input = $request->getParsedBody();
+	$sql = "INSERT INTO accounts (userID, lastName, firstName, userName, pass, points) VALUES (:userID, :lastName, :firstName, :userName, :pass, :points)";
+	$sth = $this->db->prepare($sql);
+	$uID = floor(rand(0,10000));
+	$existCheck = "SELECT userID FROM accounts WHERE userID = '$uID'";
+	while(true){
+		$result = $this->db->prepare($existCheck);
+		$result->execute();
+		if($result->rowCount() != 0)
+		{
+			$uID = floor(rand(0,10000));
+			$existCheck = "SELECT userID FROM accounts WHERE userID = '$uID'";
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+	$sth->bindParam("userID", $uID);
+	$sth->bindParam("lastName", $input['lastName']);
+	$sth->bindParam("firstName", $input['firstName']);
+	$userNameSelected = $input['userName'];
+	$result = $this->db->prepare("SELECT userName FROM accounts WHERE userName = '$userNameSelected'");
+	$result->execute();
+	if($result->rowCount() != 0)
+	{
+		echo "Username already taken!";
+		//$userNameSelected = $input['userName'];
+	}
+	$sth->bindParam("userName", $userNameSelected);
+	$sth->bindParam("pass", $input['pass']);
+	$pts = 0;
+	$sth->bindParam("points",$pts);
+	$sth->execute();
+	return $this->response->withJson($input);
+});
+
+$app->put('/updatePoints/[{accounts}]', function($request, $response){
+	$input = $request->getParsedBody();
+	$sql = "update accounts set points=:points where userID=:userID";
+	$sth = $this->db->prepare($sql);
+	$sth->bindParam("userID", $input['userID']);
+	$sth->bindParam("points", $input['points']);
+	$sth->execute();
+	return $this->response->withJson($input);
+});
+
+$app->put('/changePassword/[{accounts}]', function($request, $response){
+	$input = $request->getParsedBody();
+	$sql = "update accounts set pass=:pass where userID=:userID";
+	$sth = $this->db->prepare($sql);
+	$sth->bindParam("userID", $input['userID']);
+	$sth->bindParam("pass", $input['pass']);
+	$sth->execute();
+	return $this->response->withJson($input);
+});
+
+$app->put('/changeUserInfo/[{accounts}]', function($request, $response){
+	$input = $request->getParsedBody();
+	$sql = "update accounts set userName=:userName, lastName=:lastName, firstName=:firstName where userID=:userID";
+	$sth = $this->db->prepare($sql);
+	$sth->bindParam("userID", $input['userID']);
+	$sth->bindParam("userName", $input['userName']);
+	$sth->bindParam("lastName", $input['lastName']);
+	$sth->bindParam("firstName", $input['firstName']);
+	$sth->execute();
+	return $this->response->withJson($input);
 });
