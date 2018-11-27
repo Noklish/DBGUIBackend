5b6cae999e584b6ad6cf027fcac9c0ed3b97ef33
Noah Schweser <nschweser@smu.edu>
2018-11-26 21:47:32 -0600

    cors

--- SlimDataPHP/src/routes.php
+++ SlimDataPHP/src/routes.php
@@ -5,6 +5,18 @@ use \Firebase\JWT\JWT;
 header("Access-Control-Allow-Origin: *");
 // Routes
 
+$app->options('/{routes:.+}', function($request, $response, $args){
+	return $response;
+	});
+	
+	$app->add(function ($req, $res, $next) {
+	$response = $next($req, $res);
+	return $response
+	->withHeader('Access-Control-Allow-Origin', '*')
+	->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+	->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	});
+
 $app->get('/',function ($request, $response, $args){
 	return "Welcome to Anchor Management! This is the homepage, and there's nothing here!";
 });
